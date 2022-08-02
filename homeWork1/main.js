df6bca561a3d57507d9be34511c1cfb34448c2ff
madina155 <nurlanbekkyzy1881@gmail.com>
2022-08-02 14:55:31 +0600

    push homeWork

---
+++ homeWork1/main.js
@@ -0,0 +1,63 @@
+//Домашняя работа
+
+// 1.
+// let number = 15
+// console.log(number)
+
+// 2.
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log(a + b + c)
+
+//3.
+// let num2 = 100
+// console.log(number2 * 2)
+
+//4.
+// let d = 67
+// let e = 7
+// console.log(d % e)
+
+//5.
+// let f = 10
+// let g = 2
+// console.log(f + g)
+// console.log(f - g)
+// console.log(f * g)
+// console.log(f / g)
+
+
+//6.
+// let h = 15
+// let i = 2
+// let result = c + d
+// console.log(result)
+
+//7.
+// let j = 30
+// let k = 15
+// let l = j + k
+// console.log(j + k)
+// console.log(l % 4)
+
+//8.
+// let m = 17
+// let n = 10
+// let o = m - n
+// let p = 7
+// let result = o + p
+// console.log(result)
+
+//9.
+// let q = '10'
+// let r = '99'
+// let result = q + r
+// console.log(result)
+
+
+//10.
+// let name = 'лет'
+// let number4 = 10
+// let result = name + number4
+// console.log(result)
---
+++ homeWork3/main.js
@@ -0,0 +1,84 @@
+//1
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130,
+// }
+// let sum = 0;
+// for (let key in salaries) {
+//     sum += salaries[key];
+// }
+// console.log(sum)
+
+//2
+// let obj = {
+//     name: 'Ivan',
+//     age: 26,
+// }
+// console.log(Object.values(obj))
+
+//3
+// let obj2 = {
+//     name : 'Ivan',
+//     age : 26,
+// }
+// obj2.age = 31
+// console.log(obj2)
+
+//4
+// let obj3 = {
+//     name:'Ivan',
+//     age:26,
+//     student:true,
+// }
+// obj3.student = false
+// console.log(obj3)
+
+//5
+// let obj4 = {
+//     name: 'ABRACADABRA',
+//     age: 15,
+//     student: true
+// }
+// console.log(obj4.name.includes('BRAC'))
+
+//6
+
+
+//7
+// let obj6 = {
+//     name:'Ivan',
+//     age:26,
+//     student:true,
+// }
+//
+// let obj7 = JSON.parse(JSON.stringify(obj6))
+//     obj7.age = 50
+//     obj7.name = 'Thomas'
+//     obj7.student = false
+//
+//     console.log(obj6)
+//     console.log(obj7)
+
+//8
+// let cars = ['bmw', 'honda','mers','lexus']
+// cars.pop()
+// console.log(cars)
+
+//9
+// let cars2 = ['bmw', 'honda','mers','lexus']
+// console.log(cars2 [3])
+
+//10
+// let cars3 = ['bmw', 'honda','mers','lexus']
+// cars3[1] = 'toyota'
+// console.log(cars3)
+
+//11
+// let cars4 = ['bmw', 'honda', 'mers', 'lexus']
+// cars4.unshift('kia')
+// console.log(cars4)
+
+//12
+// let cars5 = ['bmw', 'honda','mers','lexus']
+// console.log(cars5.length)
