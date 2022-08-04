ce69ddf945c7eaee5279d81abfa1d05a29f355a8
madina155 <nurlanbekkyzy1881@gmail.com>
2022-08-04 18:00:05 +0600

    push 4homework

---
+++ homeWork4/main.js
@@ -0,0 +1,100 @@
+//1
+// let a = 'test'
+// if (a === 'test' ) {
+//     console.log('верно')
+// } else {
+//     console.log('не верно')
+// }
+
+
+//2
+// let number
+//     data = false
+// if (data === true) {
+//     number = 2
+// } else {
+//     number = 3
+// }
+// console.log(number)
+
+
+// data === true ? console.log(number) : console.log(number)
+
+
+//3
+// let time = '9:00 12:00'
+// switch (time) {
+//     case  time > '23:00' || time < '9:00' :
+//         console.log('В это время мы не работаем')
+//         break
+//     case '9:00 12:00':
+//         console.log('Будем рады вас видеть')
+//         break
+//     case '12:00 13:00':
+//         console.log('У нас обед')
+//         break
+//     case '13:00 23:00':
+//         console.log('Будем рады вас видеть')
+// }
+// prompt('Когда вы собираеттесь прийти в магазин?')
+
+//4
+// let m = 75
+// let n = 35
+//
+// if (m > 50){
+//     console.log('большой')
+// } else {
+//     console.log('маленький')
+// }
+
+//6
+// confirm('Сколько вам лет?')
+// let age = 15
+// if (age > 18) {
+//     console.log('есть')
+// } else {
+//     console.log('нет')
+// }
+
+//7
+// let a  = 1
+// let b = 4
+// if (a <= 1 || b >= 3){
+//     console.log(a + b)
+// } else {
+//     console.log(b - a)
+// }
+
+//8
+// let name = 17
+// if (name <= 17) {
+//     console.log('Он еще ребенок')
+// } else {
+//     console.log('Он уже врослый')
+// }
+
+//9
+// let season = 'май'
+// switch (season)  {
+//     case 'декабрь':
+//     case 'январь':
+//     case 'февраль':
+//         console.log('зима')
+//         break
+//     case 'апрель':
+//     case 'март':
+//     case 'май':
+//         console.log('весна')
+//         break
+//     case 'июнь':
+//     case 'июль':
+//     case 'август':
+//         console.log('лето')
+//         break
+//     case  'сентябрь':
+//     case 'октябрь':
+//     case 'ноябрь':
+//         console.log('осень')
+// }
+//10
